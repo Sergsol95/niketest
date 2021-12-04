136f11b09d87a1a081c4bbdcaf4cd2176ef530bd
Sergey Solovev <sergsol@MacBook-Air-Sergey.local>
2021-12-04 19:55:22 +0300

    second

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Index from './index.scss';
 import nikeLogo from "../../img/nike-logo.svg";
 import cart from "../../img/cart.svg";
@@ -12,7 +12,22 @@ import left from "../../img/left.svg";
 import right from "../../img/right.svg";
 
 
-function Header () {return (
+function Header () {
+
+    const itemSizeCheck = document.querySelectorAll('.item__size__input:checked')
+
+    const [count, setCount] = useState(0);
+    function increment () {
+            if (itemSizeCheck) {
+                setCount(count + 1)
+            }
+            else setCount(count)
+        }
+    
+    
+    
+    
+    return (
     <body class="body">
         <header className="header">
         <img src={nikeLogo} alt="" className="logo"/>
@@ -28,10 +43,13 @@ function Header () {return (
         <div className="info">
             <img src={profile} alt="" className="profile"/>
             <img src={cart} alt="" className="cart"/>
+            <div className="cart__shell">
+                <span className="cart__counter">{count}</span>
+            </div>
         </div>
     </header>
     <div className="info">
-    <section className="section item_section">
+    <section className="section section_item">
         <h2 className="item__classification">
             MEN'S SHOE
         </h2>
@@ -49,22 +67,52 @@ function Header () {return (
     </section>
     <section className="section section_pick">
         <div className="section__size">
-        <h2 className="item__classification">SELECT SIZE</h2>
-        <div className="item__sizepick">
-            <div className="item__size"><span className="item__size_number">8</span></div>
-            <div className="item__size"><span className="item__size_number">8.5</span></div>
-            <div className="item__size"><span className="item__size_number">9</span></div>
-            <div className="item__size"><span className="item__size_number">9.5</span></div>
-            <div className="item__size"><span className="item__size_number">10</span></div>
-            <div className="item__size"><span className="item__size_number">10.5</span></div>
-            <div className="item__size"><span className="item__size_number">11</span></div>
-            <div className="item__size"><span className="item__size_number">11.5</span></div>
-            <div className="item__size"><span className="item__size_number">12</span></div>
-            <div className="item__size"><span className="item__size_number">12.5</span></div>
-        </div>
+            <h2 className="item__classification">SELECT SIZE</h2>
+            <div className="item__sizepick">
+                <div className="item__size">
+                    <input type="radio" id="size8" class="item__size__input"/>
+                    <label for="size8" className="item__size_number">8</label>
+                </div>
+                <div className="item__size">
+                    <input type="radio" id="size8.5" class="item__size__input"/>
+                    <label for="size8.5" className="item__size_number">8.5</label>
+                </div>
+                <div className="item__size">
+                    <input type="radio" id="size9" class="item__size__input"/>
+                    <label for="size9" className="item__size_number">9</label>
+                </div>
+                <div className="item__size">
+                    <input type="radio" id="size9.5" class="item__size__input"/>
+                    <label for="size9.5" className="item__size_number">9.5</label>
+                </div>
+                <div className="item__size">
+                    <input type="radio" id="size10" class="item__size__input"/>
+                    <label for="size10" className="item__size_number">10</label>
+                </div>
+                <div className="item__size">
+                    <input type="radio" id="size10.5" class="item__size__input"/>
+                    <label for="size10.5" className="item__size_number">10.5</label>
+                </div>
+                <div className="item__size">
+                    <input type="radio" id="size11" class="item__size__input"/>
+                    <label for="size11" className="item__size_number">11</label>
+                </div>
+                <div className="item__size">
+                    <input type="radio" id="size11.5" class="item__size__input"/>
+                    <label for="size11.5" className="item__size_number">11.5</label>
+                </div>
+                <div className="item__size">
+                    <input type="radio" id="size12" class="item__size__input"/>
+                    <label for="size12.5" className="item__size_number">12</label>
+                </div>
+                <div className="item__size">
+                    <input type="radio" id="size12.5" class="item__size__input"/>
+                    <label for="size12.5" className="item__size_number">12.5</label>
+                </div>
+            </div>
         </div>
         <div className="section__color">
-        <h2 className="item__classification">SELECT COLOR</h2>
+    <h2 className="item__classification">SELECT COLOR</h2>
         <div className="item__colorpick">
             <div className="item__color item__color_brown"></div>
             <div className="item__color item__color_black"></div>
@@ -97,12 +145,12 @@ function Header () {return (
             </div>
         </div>
         <div className="footer__cart">
-            <span>ADD TO CART - </span>
+            <button onClick={increment} className="button button_cart">ADD TO CART - </button>
             <div> $95.97</div>
         </div>
     </footer>
     </body>
 )
-}
+    }
 
 export default Header;
